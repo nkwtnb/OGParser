53d5378be78024e7a46bc7fe916660916cc65058
nkwtnb <n.watanabe0305@gmail.com>
2022-03-07 14:59:29 +0900

    remove unused function

---
+++ src/OGPError.php
@@ -0,0 +1,11 @@
+<?php
+namespace nkwtnb\ogparser;
+
+class OGPError {
+  // メッセージ
+  public static $MSG = [
+    "ONLY_HTTPS" => "you can access only HTTPS sites.",
+    "SITE_NOT_FOUND" => "site not found.",
+    "PAGE_NOT_FOUND" => "page not found.",
+  ];
+}
