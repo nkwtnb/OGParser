a976d46f7cd56ee19151e43e73c5bba316d87e55
nkwtnb <n.watanabe0305@gmail.com>
2022-03-07 15:00:03 +0900

    update test

--- tests/OgparserTest.php
+++ tests/OgparserTest.php
@@ -2,29 +2,49 @@
 
 namespace nkwtnb\ogparser\test;
 
+use Exception;
 use nkwtnb\ogparser\Ogparser;
 use PHPUnit\Framework\TestCase;
 
 class OgparserTest extends TestCase {
-  protected static $ogp;
-  /**
-   * URLが存在する
-   */
-  public function existsUrlTest() {
+  // URL形式ではない
+  public function testInvalidURL() {
+    $this->expectException(Exception::class);
+    new Ogparser("abcde");
+  }
+  // HTTPSではない
+  public function testNotHttpsURL() {
+    $this->expectException(Exception::class);
+    new Ogparser("http://localhost");
+  }
+  // サイトが存在しない
+  public function testNotExistsURL() {
+    $this->expectException(Exception::class);
+    $ogp = new Ogparser("https://localhost-not-exists");
+    $ogp->fetch();
+  }
+  // ページが存在しない
+  public function testNotExistsPage() {
+    $this->expectException(Exception::class);
+    $ogp = new Ogparser("https://www.google.com/not-exists");
+    $ogp->fetch();
   }
   /**
-   * タイトルが取得できる
+   * @doesNotPerformAssertions
    */
-  public function canGetTitleTest() {
-  }
-  /**
-   * 説明が取得できる
-   */
-  public function canGetDescriptionTest() {
-  }
-  /**
-   * 画像が取得できる
-   */
-  public function canGetImageTest() {
+  // OGPの取得処理が実行できる
+  public function testCanGetOGP() {
+    $_URL = getenv("URL");
+    throw new Exception("specify target url. ex)URL=https://www.example.com phpunit tests");
+    $ogp = new Ogparser($_URL);
+    $ogp->fetch();
+    var_dump(<<<EOM
+
+{$ogp->get_url()}
+{$ogp->get_title()}
+{$ogp->get_description()}
+{$ogp->get_image()}
+{$ogp->get_site_name()}
+EOM);
   }
 }
